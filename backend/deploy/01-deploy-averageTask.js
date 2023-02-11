b9e0a6ed4f9418812bacda0681d496f6a1e26299
Adrian <a.neiser13@gmail.com>
2023-02-11 22:02:49 +0100

    Adjust deployment script to mint 10000 mockDAI

--- backend/deploy/01-deploy-averageTask.js
+++ backend/deploy/01-deploy-averageTask.js
@@ -1,10 +1,13 @@
 const { network } = require("hardhat")
+const { developmentChains } = require("../helper-hardhat-config")
 
 module.exports = async({ getNamedAccounts, deployments }) => {
     const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts()
 
-    log("--------------------------------------")
+    log("----------------------------------------------------------------------")
+    log("01-deploy-averageTask.js")
+    log("----------------------------------------------------------------------")
     arguments = []
     const AveragingStrategy = await deploy("AveragingStrategy", {
         from: deployer,
@@ -12,6 +15,16 @@ module.exports = async({ getNamedAccounts, deployments }) => {
         log: true,
         waitConfirmations: network.config.blockConfirmations || 1
     })
+
+    // If deploying to localhost, (for dev/testing purposes) need to deploy own ERC20
+    if (developmentChains.includes(network.name)) {
+        const MockDaiTokenContract = await hre.ethers.getContractFactory("MockDaiToken");
+        mockDai = await MockDaiTokenContract.deploy();
+        await mockDai.deployed()
+        let DAITokenAddress = mockDai.address
+        console.log("mockDai address: " + DAITokenAddress);
+    }
+    log("======================================================================")
 }
 
 module.exports.tags = ["all", "averagingStrategy", "main"]
