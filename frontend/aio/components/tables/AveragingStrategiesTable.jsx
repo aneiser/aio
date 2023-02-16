cc74ed20828c9782c6bcd37bfc3c08d4d79aecfc
Adrian <a.neiser13@gmail.com>
2023-02-16 19:30:02 +0100

    Fill averaging strategies table with raw data
    Add 
    - a switch to indicate status and future edition
    - placeholder for token image

--- frontend/aio/components/tables/AveragingStrategiesTable.jsx
+++ frontend/aio/components/tables/AveragingStrategiesTable.jsx
@@ -17,6 +17,10 @@ import {
     TableCaption,
     TableContainer,
 } from '@chakra-ui/react'
+import { Switch } from '@chakra-ui/react'
+import { Text } from '@chakra-ui/react'
+import { Stack, HStack, VStack } from '@chakra-ui/react'
+import { Avatar, AvatarBadge, AvatarGroup } from '@chakra-ui/react'
 // Components & Dapp contracts
 import AveragingStrategyContract from 'public/AveragingStrategy.json'
 
@@ -120,35 +124,59 @@ export function AveragingStrategiesTable() {
                 <TableCaption>Defined dollar cost averaging strategies</TableCaption>
                 <Thead>
                     <Tr key="tokenAddress_1">
-                        <Th>Status</Th>{/* <Th>isActive</Th> */}
-                        <Th>Token</Th>{/* <Th>tokenName</Th> */}
-                        <Th isNumeric>DCA amount</Th>{/* <Th>amount</Th> */}
-                        <Th isNumeric>DCA interval</Th>{/* <Th>frequency</Th> */}
-                        <Th isNumeric>Current price</Th>
+                        <Th>Status</Th>
+                        <Th>From</Th>
+                        <Th>To</Th>
+                        <Th isNumeric>Amount</Th>
+                        <Th isNumeric>Interval</Th>
+                        {/* <Th isNumeric>Current price</Th>
                         <Th isNumeric>Avg. price</Th>
-                        <Th isNumeric>Diff</Th>
+                        <Th isNumeric>Diff</Th> */}
                     </Tr>
                 </Thead>
                 <Tbody>
-                    <Tr key="tokenAddress_2">
-                        <Td>Active</Td>
-                        <Td>ETH</Td>
-                        <Td isNumeric>10 $</Td>
-                        <Td isNumeric>1 week</Td>
-                        <Td isNumeric>1600 $</Td>
+                    {strategiesList && strategiesList.map((strategy, index) =>
+                    <Tr key={index}>
+                        <Td>
+                            <Switch
+                                // id="initial-status"
+                                isChecked={strategy.isActive}
+                                isDisabled
+                                // onChange={(e) => setSelectedInitialStatus(e.target.checked)}
+                            />
+                        </Td>
+                        <Td>
+                            <HStack>
+                                {/* <SkeletonCircle isLoaded={!isLoadingSelectedSourceToken}> */}
+                                    <Avatar
+                                        size="sm"
+                                        // name={selectedSourceToken ? selectedSourceToken.symbol : ''}
+                                        // src={selectedSourceToken ? selectedSourceToken.icon : ''}
+                                    />
+                                {/* </SkeletonCircle> */}
+                                    <Text>{strategy.sourceTokenAddress}</Text>
+                            </HStack>
+                        </Td>
+                        <Td>
+                            <HStack>
+                                {/* <keletonCircle isLoaded={!isLoadingSelectedSourceToken}> */}
+                                    <Avatar
+                                        size="sm"
+                                        // name={selectedSourceToken ? selectedSourceToken.symbol : ''}
+                                        // src={selectedSourceToken ? selectedSourceToken.icon : ''}
+                                    />
+                                {/* </SkeletonCircle> */}
+                                    <Text>{strategy.tokenToAverageAddress}</Text>
+                            </HStack>
+                        </Td>
+                        <Td isNumeric>{strategy.amount}</Td>
+                        <Td isNumeric>{strategy.frequency}</Td>
+                        {/* <Td isNumeric>1600 $</Td>
                         <Td isNumeric>1300 $</Td>
-                        <Td isNumeric>10 %</Td>
+                        <Td isNumeric>10 %</Td> */}
                     </Tr>
-                    <Tr key="tokenAddress_3">
-                        <Td>Active</Td>
-                        <Td>ETH</Td>
-                        <Td isNumeric>10 $</Td>
-                        <Td isNumeric>1 week</Td>
-                        <Td isNumeric>1600 $</Td>
-                        <Td isNumeric>1300 $</Td>
-                        <Td isNumeric>10 %</Td>
-                    </Tr>
-                    <Tr key="tokenAddress_4">
+                    )}
+                    <Tr color="blue" key="tokenAddress_4">
                         <Td>Active</Td>
                         <Td>ETH</Td>
                         <Td isNumeric>10 $</Td>
