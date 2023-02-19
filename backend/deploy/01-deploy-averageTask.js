98e9a9987aed79de80663e1b011335947a8e23b2
Adrian <a.neiser13@gmail.com>
2023-02-19 22:00:35 +0100

    Add deployment script for AveragingStrategyUpkeepRunner

--- backend/deploy/01-deploy-averageTask.js
+++ backend/deploy/01-deploy-averageTask.js
@@ -21,6 +21,12 @@ module.exports = async({ getNamedAccounts, deployments }) => {
         log: true,
         waitConfirmations: network.config.blockConfirmations || 1
     })
+    const AveragingStrategyUpkeepRunner = await deploy("AveragingStrategyUpkeepRunner", {
+        from: deployer,
+        args: arguments,
+        log: true,
+        waitConfirmations: network.config.blockConfirmations || 1
+    })
 
     // If deploying to localhost, (for dev/testing purposes) need to deploy own ERC20
     if (developmentChains.includes(network.name)) {
