7c14f0a40be6b95f266c51f69aced7904c234d98
Adrian <a.neiser13@gmail.com>
2023-02-19 08:44:50 +0100

    Comment Chainlink "UpkeepIDConsumerExample" for clarification

--- backend/contracts/UpkeepIDConsumerExample.sol
+++ backend/contracts/UpkeepIDConsumerExample.sol
@@ -38,9 +38,9 @@ contract UpkeepIDConsumerExample {
         address _registrar,
         AutomationRegistryInterface _registry
     ) {
-        i_link = _link;
-        registrar = _registrar;
-        i_registry = _registry;
+        i_link = _link;         // GOERLI_LINK_ADDRESS:      0x326C977E6efc84E512bB9C30f76E30c160eD06FB (https://docs.chain.link/resources/link-token-contracts/)
+        registrar = _registrar; // GOERLI_REGISTRAR_ADDRESS: 0x02777053d6764996e594c3E88AF1D58D5363a2e6
+        i_registry = _registry; // GOERLI_REGISTRY_ADDRESS:  0x9806cf6fBc89aBF286e8140C42174B94836e36F2
     }
 
     function registerAndPredictID(
@@ -53,29 +53,47 @@ contract UpkeepIDConsumerExample {
         uint96 amount,
         uint8 source
     ) public {
-        (State memory state, Config memory _c, address[] memory _k) = i_registry
-            .getState();
+        // Gets, previous to the creation of the upkeep, the state and...
+        (State memory state, Config memory _c, address[] memory _k) = i_registry.getState();
+        // ...the old nonce of the registry to later generate an upkeepID
         uint256 oldNonce = state.nonce;
+
+        // Encodes a payload to be used on the creation of the upkeep
         bytes memory payload = abi.encode(
-            name,
-            encryptedEmail,
-            upkeepContract,
-            gasLimit,
-            adminAddress,
-            checkData,
-            amount,
-            source,
-            address(this)
+            // Official Chainlink registerAndPredictID Parameters (https://docs.chain.link/chainlink-automation/register-upkeep/#register-an-upkeep-using-your-own-deployed-contract)
+            // ----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+            // name           Name of Upkeep
+            // encryptedEmail Not in use in programmatic registration. Please specify with 0x
+            // upkeepContract Address of Keepers-compatible contract that will be automated
+            // gasLimit       The maximum amount of gas that will be used to execute your function on-chain
+            // adminAddress   Address for Upkeep administrator. Upkeep administrator can fund contract.
+            // checkData      ABI-encoded fixed and specified at Upkeep registration and used in every checkUpkeep. Can be empty (0x)
+            // amount         The amount of LINK (in Wei) to fund your Upkeep. The minimum amount is 5 LINK. To fund 5 LINK please set this to 5000000000000000000
+            // source         Not in use in programmatic registration. Please specify with 0.
+            name,          // name           Will be the averagingStrategyId passed as argument
+            encryptedEmail,// encryptedEmail = hex""
+            upkeepContract,// upkeepContract The compatible Upkeep Contract address to automate. The one with checkUpkeep() and performUpkeep(). More info:https://docs.chain.link/chainlink-automation/compatible-contracts
+            gasLimit,      // gasLimit       = 3000000 for example, later could be parametrized
+            adminAddress,  // adminAddress   = msg.sender Your wallet address from which you've deployed, sent LINK etc.
+            checkData,     // checkData      = hex""?
+            amount,        // amount         = 5000000000000000000 (the min)
+            source,        // source         = 0
+            address(this)   // sender         this is the UpkeepIDConsumerExample's address. In this example it's the calling contract itself.
         );
 
+        // Creates the upkeep
         i_link.transferAndCall(
             registrar,
             amount,
             bytes.concat(registerSig, payload)
         );
+
+        // Get, after the creation of the upkeep, the state and...
         (state, _c, _k) = i_registry.getState();
+        // ...the new nonce of the registry to...
         uint256 newNonce = state.nonce;
         if (newNonce == oldNonce + 1) {
+            // ...generate a random upkeepID
             uint256 upkeepID = uint256(
                 keccak256(
                     abi.encodePacked(
@@ -86,6 +104,7 @@ contract UpkeepIDConsumerExample {
                 )
             );
             // DEV - Use the upkeepID however you see fit
+            // @TODO añadirlo a la configuración d ela estreategia?
         } else {
             revert("auto-approve disabled");
         }
